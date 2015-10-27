480c0cbbbaeb46290ec495a492e917ce15dd2291
Tim Padjen <tpadjen@gmail.com>
2015-10-27 03:52:10 -0600

    Test APP_ID

---
+++ src/app/test/app_id.spec.ts
@@ -0,0 +1,17 @@
+import {
+	it,
+	describe,
+	expect,
+	inject
+} from 'angular2/testing';
+
+import {APP_ID} from 'angular2/angular2';
+
+
+describe('default test injector', () => {
+
+	it('should provide id', inject([APP_ID], (id) => {
+		expect(id).toBe('a');
+	}));
+
+});
