aeeca14bb54dd1fef452e3197a48083e4a18b3a5
Tim Padjen <tpadjen@gmail.com>
2015-10-27 06:48:09 -0600

    Test DraftBoardPage

---
+++ src/app/components/pages/draft_board/draft_board.spec.ts
@@ -0,0 +1,37 @@
+import {
+	it,
+	describe,
+	expect,
+	injectAsync,
+	beforeEachProviders,
+	TestComponentBuilder
+} from 'angular2/testing';
+
+import {DraftBoardPage} from './draft_board';
+import {DraftBoardComponent} from '../../draft/board/board';
+import {Component, provide} from 'angular2/angular2';
+
+@Component({
+	selector: 'draft-board',
+	template: '<div id="draft-board>This is the draft board</div>'
+})
+class MockDraftBoardComponent { };
+
+
+describe('DraftBoardPage', () => {
+
+	beforeEachProviders(() => [DraftBoardPage]);
+
+	it('should display a draft board', injectAsync([TestComponentBuilder], (tcb) => {
+		return tcb.overrideDirective(DraftBoardPage, DraftBoardComponent, MockDraftBoardComponent)
+			.createAsync(DraftBoardPage).then((fixture) => {
+			
+			fixture.detectChanges();
+
+			var compiled = fixture.debugElement.nativeElement;
+			expect(compiled.querySelector('draft-board')).not.toBeNull();
+		});
+	}));
+
+
+});
