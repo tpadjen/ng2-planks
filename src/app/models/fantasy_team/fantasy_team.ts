668f54c3851212dad93babfb19c3d8b208fce4c1
Tim Padjen <tpadjen@gmail.com>
2015-10-31 06:44:49 -0600

    Remove draft components

--- src/app/models/fantasy_team/fantasy_team.ts
+++
@@ -1,5 +0,0 @@
-import {NFLPlayer} from '../nfl_player/nfl_player';
-
-export class FantasyTeam {
-  owner: string;
-}
--- src/app/models/nfl_player/nfl_player.ts
+++
@@ -1,7 +0,0 @@
-import {NFLTeam} from '../nfl_team/nfl_team';
-
-export class NFLPlayer {
-  name: string;
-  position: string;
-  team: NFLTeam;
-}
