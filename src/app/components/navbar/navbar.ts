1323b9c2147a9882317683001079158731765038
Tim Padjen <tpadjen@gmail.com>
2015-11-05 12:41:13 -0700

    User menu

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,13 @@
 import {Component, NgIf, NgFor} from 'angular2/angular2';
 import {RouterLink} from 'angular2/router';
 
+import {dropdown} from 'ng2-bootstrap/ng2-bootstrap';
+
 import {UserService} from '../../services/user-service';
 
 @Component({
   selector: 'navbar',
-  directives: [RouterLink, NgIf, NgFor],
+  directives: [RouterLink, NgIf, NgFor, dropdown],
   pipes: [],
   styleUrls: ['app/components/navbar/navbar.css'],
   templateUrl: 'app/components/navbar/navbar.html'
