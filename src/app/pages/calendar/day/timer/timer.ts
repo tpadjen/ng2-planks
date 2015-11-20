24dcb8b5b9fd3e922d9f7cd1db2665405729b3b8
Tim Padjen <tpadjen@gmail.com>
2015-11-20 02:06:18 -0700

    Timer on day modal

--- src/app/pages/calendar/day/timer/timer.ts
+++ src/app/pages/calendar/day/timer/timer.ts
@@ -1,17 +1,42 @@
-import {Component} from 'angular2/angular2';
+import {Component, NgIf} from 'angular2/angular2';
+
+import {ToTimePipe} from './pipes/to-time';
 
 let styles = require('./timer.css');
 let template = require('./timer.html');
 
 @Component({
   selector: 'timer',
-  directives: [],
-  pipes: [],
+  directives: [NgIf],
+  pipes: [ToTimePipe],
   styles: [styles],
   template: template
 })
 export class Timer {
+  time: number = 0;
+  playing = null;
 
   constructor() {}
+
+  play() {
+    this.playing = setInterval(() => {
+      this.time += 0.1;
+    }, 100);
+  }
+
+  togglePlay(event) {
+    event.stopPropagation();
+    this.playing == null ? this.play() : this.pause();
+  }
+
+  stop(event) {
+    this.pause();
+    this.time = 0;
+  }
+
+  pause() {
+    clearTimeout(this.playing);
+    this.playing = null;
+  }
 
 }
